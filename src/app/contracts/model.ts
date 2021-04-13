f8df31f67b1bfb9416c7737cfd4c4b6c2e7ad3e4
jonskyy <48381284+jonskyy@users.noreply.github.com>
2021-04-13 18:13:36 +0200

    Delete src directory

--- src/app/contracts/model.ts
+++
@@ -1,7 +0,0 @@
-export interface Contract {
-    id: number;
-    customerName: string;
-    customerId: number;
-    value: number;
-    date: number;
-}
--- src/app/model.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export interface Config {
-    customerLimit: number;
-    apiUrl:String;
-}
-
-export const CONFIG = new InjectionToken<Config>('app.config');
